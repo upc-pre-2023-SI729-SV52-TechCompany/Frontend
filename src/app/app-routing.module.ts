e973d1630c5c7b829dfe58cb75d905f259875e25
Damarists <111830490+Damarists@users.noreply.github.com>
2023-09-30 01:06:33 -0500

    Update app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,6 +5,10 @@ import {ProfileComponent} from "./profile/pages/profile/profile.component";
 import {ContractsComponent} from "./contracts/pages/contracts/contracts.component";
 import {SupportComponent} from "./support/pages/support/support.component";
 import {SearchVehicleComponent} from "./search-vehicle/pages/search-vehicle/search-vehicle.component";
+import { LoginComponent} from "./LoginStart/pages/login/login.component";
+import { RegistroComponent} from "./LoginStart/pages/registro/registro.component";
+import { RegistroPaso2Component} from "./LoginStart/pages/registro-paso2/registro-paso2.component";
+import { RegistroPaso3Component} from "./LoginStart/pages/registro-paso3/registro-paso3.component";
 
 const routes: Routes = [
   {path: 'home', component: HomeComponent},
@@ -12,6 +16,10 @@ const routes: Routes = [
   {path: 'search-vehicle', component: SearchVehicleComponent},
   {path: 'contracts', component: ContractsComponent},
   {path: 'support', component: SupportComponent},
+  { path: 'login', component: LoginComponent },
+  { path: 'registro', component: RegistroComponent },
+  { path: 'registro/paso2', component: RegistroPaso2Component },
+  { path: 'registro/paso3', component: RegistroPaso3Component },
   {path: '', redirectTo: '/home', pathMatch: 'full'},
 ];
 
